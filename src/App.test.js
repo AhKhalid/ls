05cca000b9be8d9811f8dd8999c36d7ef1104d7c
Ahmed Abdelgadir <ah.khalid.elamin@hotmail.com>
2023-11-12 22:16:59 +0400

    Fix the dummy test.

--- src/App.test.js
+++ src/App.test.js
@@ -2,5 +2,5 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  expect(sum(1, 2)).toEqual(3);
+  expect(3).toEqual(3);
 });
